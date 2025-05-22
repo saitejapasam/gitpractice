df3121a301e8fde9efb0210b3a90280b861d35b2
saitejapasam <pasamsai94@gmail.com>
2025-05-22 16:09:09 -0400

    Update 01-hello-world.sh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Ctrl+S --> save, this is only for info purpose. not part of code
-# Author - Sivakumar Reddy
-# Team - DevOps
+# Author - Sai Teja Pasam
+# Team - Sai
 echo "Hello world"
